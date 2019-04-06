8142c14268f5ba359ba01167907d55086c08edb5
Carlos Montiers A <cmontiers@gmail.com>
2019-04-06 12:48:16 -0300

    Diccionarios

---
+++ lenguaje/diccionario.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding: utf-8
+
+undiccionario = {
+    "year": 1964,
+    "brand": "Ford",
+    "model": "Mustang",
+}
+print(undiccionario)
+
+# 2.7 insertion unordered: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+# 3.6 insertion ordered  : {'year': 1964, 'brand': 'Ford', 'model': 'Mustang'}
